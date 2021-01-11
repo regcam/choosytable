4974b796fc8a10866209e179f1837a5cb71be267
Reginald Campbell <reginaldcampbell@Reginalds-MacBook-Air.local>
2021-01-11 14:44:41 -0500

    can't access API without logging in

--- mongo.py
+++ mongo.py
@@ -6,6 +6,7 @@ from flask_pymongo import PyMongo, ObjectId
 from bson import json_util
 from datetime import datetime
 import urllib.request,urllib.parse,urllib.error
+import os
 
 
 # This is one of the Redirect URIs from Google APIs console
@@ -13,15 +14,15 @@ REDIRECT_URI = '/oauth2callback'
 
 # Add your SECRET_KEY here
 # Set your secret key as an evironment variable and use it as below
-SECRET_KEY = ""
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 app = Flask(__name__)
 
 # Your GOOGLE_CLIENT_ID and GOOGLE_CLIENT SECRET are needed here
 # You can get them at https://code.google.com/apis/console
-GOOGLE_CLIENT_ID = ""
-GOOGLE_CLIENT_SECRET = ""
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 
 app.config['MONGO_DBNAME'] = 'restdb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
@@ -44,37 +45,49 @@ access_token_params={'grant_type': 'authorization_code'},
 consumer_key=GOOGLE_CLIENT_ID,
 consumer_secret=GOOGLE_CLIENT_SECRET)
 
-@app.route("/")
-def home():
-  access_token = session.get('access_token')
-  if access_token is None:
-      return redirect(url_for('login'))
-  
-  access_token = access_token[0]
-
-  headers = {'Authorization': 'OAuth '+access_token}
-  req = urllib.request.Request('https://www.googleapis.com/oauth2/v1/userinfo',
-                None, headers)
-  try:
-    res = urllib.request.urlopen(req)
-  except urllib.error.HTTPError as e:
-    if e.code == 401:
-        # Unauthorized - bad token
-        session.pop('access_token', None)
-        return redirect(url_for('login'))
-    return res.read()
-
-@app.route('/welcome')
-def welcome():
+def Notloggedin():
   html_str = '''
   <!DOCTYPE html>
   <html lang="en">
   '''
-  html_str = html_str + "\n<h1>Choosy Table</h1>\n"
-  html_str = html_str + "\n### " + str(db) + "\n"
-  html_str = html_str + "\n### " + str(star) + "\n"
-  html_str = html_str + "\n<h2>Latest Info:</h2><h3>" + str(star.find_one()) + "</h3>\n</html>"
+  html_str = html_str + "\n<h1>Welcome to Choosy Table</h1>\n"
+  html_str = html_str + "\n<h2>Please <a href=login>login</a></h2>\n</html>"
   return html_str
+
+@app.route("/")
+def home():
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  elif access_token:
+    access_token = access_token[0]
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = urllib.request.Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+      res = urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+      if e.code == 401:
+          # Unauthorized - bad token
+          session.pop('access_token', None)
+          return redirect(url_for('login'))
+      return res.read()
+
+@app.route('/welcome')
+def welcome():
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  else:
+    html_str = '''
+    <!DOCTYPE html>
+    <html lang="en">
+    '''
+    html_str = html_str + "\n<h1>Choosy Table</h1>\n"
+    html_str = html_str + "\n### " + str(db) + "\n"
+    html_str = html_str + "\n### " + str(star) + "\n"
+    html_str = html_str + "\n<h2>Latest Info:</h2><h3>" + str(star.find_one()) + "</h3>\n</html>"
+    return html_str
 
 @app.route('/login')
 def login():
@@ -94,79 +107,111 @@ def get_access_token():
 
 @app.route('/company', methods=['GET'])
 def get_all_companies():
-  return json_util.dumps(star.find({'reviews':{"$exists":True}}))
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  else:
+    return json_util.dumps(star.find({'reviews':{"$exists":True}}))
 
 @app.route('/company', methods=['POST'])
 def add_company():
-  company = request.json['company']
-  creator = request.json['creator']
-  reviews = request.json['reviews']
-  output={}
-  try:
-      return json_util.dumps(star.insert({'created' : datetime.now(), 'company': company, 'creator': creator, 'reviews': reviews}))
-  except Exception as e:
-      output = {'error' : str(e)}
-      return jsonify(output)
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin()
+  else:
+    company = request.json['company']
+    creator = request.json['creator']
+    reviews = request.json['reviews']
+    output={}
+    try:
+        return json_util.dumps(star.insert({'created' : datetime.now(), 'company': company, 'creator': creator, 'reviews': reviews}))
+    except Exception as e:
+        output = {'error' : str(e)}
+        return jsonify(output)
 
 @app.route('/company/<company_id>', methods=['GET'])
 def get_company(company_id):
-  return json_util.dumps(star.find_one({'_id': ObjectId(company_id)}))
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  else:
+    return json_util.dumps(star.find_one({'_id': ObjectId(company_id)}))
 
 @app.route('/company/<company_id>', methods=['POST'])
 def update_company(company_id):
-  r = star.find_one({'_id' : ObjectId(company_id)})
-  if r:
-    for key in request.json.keys():
-        r[key] = request.json[key]
-    try:
-        output= star.replace_one({'_id' : ObjectId(company_id)}, r)
-        output= star.update_one({'_id' : ObjectId(company_id)}, { "$set": {'last_modified':datetime.now() } } )
-        output = {'message' : 'company updated'}
-        return jsonify({'result' : output})
-    except Exception as e:
-        output = {'error' : str(e)}
-        return jsonify(output)
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
   else:
-    output = {'error' : 'company not found'}
-    return jsonify(output)
+    r = star.find_one({'_id' : ObjectId(company_id)})
+    if r:
+      for key in request.json.keys():
+          r[key] = request.json[key]
+      try:
+          output= star.replace_one({'_id' : ObjectId(company_id)}, r)
+          output= star.update_one({'_id' : ObjectId(company_id)}, { "$set": {'last_modified':datetime.now() } } )
+          output = {'message' : 'company updated'}
+          return jsonify({'result' : output})
+      except Exception as e:
+          output = {'error' : str(e)}
+          return jsonify(output)
+    else:
+      output = {'error' : 'company not found'}
+      return jsonify(output)
 
 @app.route('/person/<person_id>', methods=['GET'])
 def get_person(person_id):
-  return json_util.dumps(star.find_one({'_id': ObjectId(person_id)}))
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  else:
+    return json_util.dumps(star.find_one({'_id': ObjectId(person_id)}))
 
 @app.route('/person/<person_id>', methods=['POST'])
 def update_person(person_id):
-  r = star.find_one({'_id' : ObjectId(person_id)})
-  if r:
-    for key in request.json.keys():
-        r[key] = request.json[key]
-    try:
-        output= star.replace_one({'_id' : ObjectId(person_id)}, r)
-        output= star.update_one({'_id' : ObjectId(person_id)}, { "$set": {'last_modified':datetime.now() } } )
-        output = {'message' : 'person updated'}
-        return jsonify({'result' : output})
-    except Exception as e:
-        output = {'error' : str(e)}
-        return jsonify(output)
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
   else:
-    output = {'error' : 'person not found'}
-    return jsonify(output)
+    r = star.find_one({'_id' : ObjectId(person_id)})
+    if r:
+      for key in request.json.keys():
+          r[key] = request.json[key]
+      try:
+          output= star.replace_one({'_id' : ObjectId(person_id)}, r)
+          output= star.update_one({'_id' : ObjectId(person_id)}, { "$set": {'last_modified':datetime.now() } } )
+          output = {'message' : 'person updated'}
+          return jsonify({'result' : output})
+      except Exception as e:
+          output = {'error' : str(e)}
+          return jsonify(output)
+    else:
+      output = {'error' : 'person not found'}
+      return jsonify(output)
 
 @app.route('/person', methods=['GET'])
 def get_all_persons():
-  return json_util.dumps(star.find({'email':{"$exists":True}}))
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  else:
+    return json_util.dumps(star.find({'email':{"$exists":True}}))
 
 @app.route('/person', methods=['POST'])
 def add_person():
-  name = request.json['name']
-  email = request.json['email']
-  ethnicity = request.json['ethnicity']
-  output={}
-  try:
-      return json_util.dumps(star.insert({'created' : datetime.now(), 'name': name, 'email': email, 'ethnicity': ethnicity}))
-  except Exception as e:
-      output = {'error' : str(e)}
-      return jsonify(output)
+  access_token = session.get('access_token')
+  if access_token is None:
+    return Notloggedin() 
+  else:
+    name = request.json['name']
+    email = request.json['email']
+    ethnicity = request.json['ethnicity']
+    output={}
+    try:
+        return json_util.dumps(star.insert({'created' : datetime.now(), 'name': name, 'email': email, 'ethnicity': ethnicity}))
+    except Exception as e:
+        output = {'error' : str(e)}
+        return jsonify(output)
 
 if __name__ == '__main__':
     app.run(debug=True)
