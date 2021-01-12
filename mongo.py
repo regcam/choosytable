c08dda8a9649d1871ddd06adf51dae8611da718d
Reginald Campbell <reginaldcampbell@Reginalds-MacBook-Air.local>
2021-01-11 19:06:34 -0500

    leverage flask_dance. move html to templates

--- mongo.py
+++ mongo.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, session
+from flask import Flask, redirect, url_for, session, render_template
 from flask_oauth import OAuth
 from flask import jsonify
 from flask import request
@@ -7,117 +7,62 @@ from bson import json_util
 from datetime import datetime
 import urllib.request,urllib.parse,urllib.error
 import os
+from flask_dance.contrib.google import make_google_blueprint, google
 
-
-# This is one of the Redirect URIs from Google APIs console
-REDIRECT_URI = '/oauth2callback'
-
-# Add your SECRET_KEY here
-# Set your secret key as an evironment variable and use it as below
-SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
-
 app = Flask(__name__)
-
-# Your GOOGLE_CLIENT_ID and GOOGLE_CLIENT SECRET are needed here
-# You can get them at https://code.google.com/apis/console
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 
 app.config['MONGO_DBNAME'] = 'restdb'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
-app.secret_key = SECRET_KEY
-oauth = OAuth()
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+app.secret_key = os.environ.get("SECRET_KEY")
+google_bp = make_google_blueprint(
+  client_id=os.environ.get("GOOGLE_CLIENT_ID"),
+  client_secret=os.environ.get("GOOGLE_CLIENT_SECRET"),
+  scope=[
+    "https://www.googleapis.com/auth/plus.me",
+    "https://www.googleapis.com/auth/userinfo.email"
+    ])
+app.register_blueprint(google_bp, url_prefix="/login")
 
 mongo = PyMongo(app)
 star = mongo.db.stars
 db = mongo.db
 
-google = oauth.remote_app('google',
-base_url='https://www.google.com/accounts/',
-authorize_url='https://accounts.google.com/o/oauth2/auth',
-request_token_url=None,
-request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
-'response_type': 'code'},
-access_token_url='https://accounts.google.com/o/oauth2/token',
-access_token_method='POST',
-access_token_params={'grant_type': 'authorization_code'},
-consumer_key=GOOGLE_CLIENT_ID,
-consumer_secret=GOOGLE_CLIENT_SECRET)
-
-def Notloggedin():
-  html_str = '''
-  <!DOCTYPE html>
-  <html lang="en">
-  '''
-  html_str = html_str + "\n<h1>Welcome to Choosy Table</h1>\n"
-  html_str = html_str + "\n<h2>Please <a href=login>login</a></h2>\n</html>"
-  return html_str
-
 @app.route("/")
 def home():
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
-  elif access_token:
-    access_token = access_token[0]
-    headers = {'Authorization': 'OAuth '+access_token}
-    req = urllib.request.Request('https://www.googleapis.com/oauth2/v1/userinfo',
-                  None, headers)
-    try:
-      res = urllib.request.urlopen(req)
-    except urllib.error.HTTPError as e:
-      if e.code == 401:
-          # Unauthorized - bad token
-          session.pop('access_token', None)
-          return redirect(url_for('login'))
-      return res.read()
+  if not google.authorized:
+    return render_template('index.html') 
+  else:
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    return redirect(url_for('welcome'))
 
 @app.route('/welcome')
 def welcome():
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html')
   else:
-    html_str = '''
-    <!DOCTYPE html>
-    <html lang="en">
-    '''
-    html_str = html_str + "\n<h1>Choosy Table</h1>\n"
-    html_str = html_str + "\n### " + str(db) + "\n"
-    html_str = html_str + "\n### " + str(star) + "\n"
-    html_str = html_str + "\n<h2>Latest Info:</h2><h3>" + str(star.find_one()) + "</h3>\n</html>"
-    return html_str
-
-@app.route('/login')
-def login():
-    callback=url_for('authorized', _external=True)
-    return google.authorize(callback=callback)
-
-@app.route(REDIRECT_URI)
-@google.authorized_handler
-def authorized(resp):
-    access_token = resp['access_token']
-    session['access_token'] = access_token, ''
-    return redirect(url_for('welcome'))
-
-#@google.tokengetter
-#def get_access_token():
-#    return session.get('access_token')
+    resp = google.get("/oauth2/v1/userinfo")
+    return render_template(
+      'welcome.html', 
+      email=resp.json()["email"], 
+      database=str(db), 
+      collection=str(star), 
+      latestInfo=str(star.find_one()))
 
 @app.route('/company', methods=['GET'])
 def get_all_companies():
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     return json_util.dumps(star.find({'reviews':{"$exists":True}}))
 
 @app.route('/company', methods=['POST'])
 def add_company():
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin()
+  if not google.authorized:
+    return render_template('index.html')
   else:
     company = request.json['company']
     creator = request.json['creator']
@@ -131,17 +76,15 @@ def add_company():
 
 @app.route('/company/<company_id>', methods=['GET'])
 def get_company(company_id):
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     return json_util.dumps(star.find_one({'_id': ObjectId(company_id)}))
 
 @app.route('/company/<company_id>', methods=['POST'])
 def update_company(company_id):
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     r = star.find_one({'_id' : ObjectId(company_id)})
     if r:
@@ -161,17 +104,15 @@ def update_company(company_id):
 
 @app.route('/person/<person_id>', methods=['GET'])
 def get_person(person_id):
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     return json_util.dumps(star.find_one({'_id': ObjectId(person_id)}))
 
 @app.route('/person/<person_id>', methods=['POST'])
 def update_person(person_id):
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     r = star.find_one({'_id' : ObjectId(person_id)})
     if r:
@@ -191,17 +132,15 @@ def update_person(person_id):
 
 @app.route('/person', methods=['GET'])
 def get_all_persons():
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     return json_util.dumps(star.find({'email':{"$exists":True}}))
 
 @app.route('/person', methods=['POST'])
 def add_person():
-  access_token = session.get('access_token')
-  if access_token is None:
-    return Notloggedin() 
+  if not google.authorized:
+    return render_template('index.html') 
   else:
     name = request.json['name']
     email = request.json['email']
