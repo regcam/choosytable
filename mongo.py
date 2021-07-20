1c531c6fd48f4d5796843f64d54a6b3e13b3f409
Reginald Campbell <reg.c.campbell@gmail.com>
2021-07-20 12:58:57 -0400

    pull google login out of homepage

--- mongo.py
+++ mongo.py
@@ -180,6 +180,43 @@ def get_pagination(**kwargs):
         **kwargs
     )
 
+def google_logged_in(blueprint, token):
+    if not token:
+        flash("Failed to log in.", category="error")
+        return False
+
+    resp = blueprint.session.get("/oauth2/v1/userinfo")
+    if not resp.ok:
+        msg = "Failed to fetch user info."
+        flash(msg, category="error")
+        return False
+
+    info = resp.json()
+    user_id = info["id"]
+
+    # Find this OAuth token in the database, or create it
+    try:
+        oauth = find_email(info['email'])
+    except:
+        #oauth = OAuth(provider=blueprint.name, provider_user_id=user_id, token=token)
+        flash("User not found")
+
+    if oauth.email:
+        login_user(oauth.email)
+        flash("Successfully signed in.")
+    else:
+        # Create a new local user account for this user
+        user = User(email=info["email"])
+        # Associate the new local user account with the OAuth token
+        oauth.user = user
+        # Save and commit our database models
+        ct.insert(oauth)
+        # Log in the new local user account
+        login_user(oauth.email)
+        flash("Successfully signed in.")
+
+    # Disable Flask-Dance's default behavior for saving the OAuth token
+    return False
 
 @app.route("/")
 @app.route("/home")
@@ -189,26 +226,11 @@ def home():
     if not google.authorized:
         return redirect(url_for("google.login"))
 
-    resp = google.get("/oauth2/v2/userinfo")
-    assert resp.ok, resp.txt
-    email=resp.json()['email']
-    blueprint.storage = MongoStorage(email)
-    token=google.token
-   
-    try:
-        user=MongoStorage.get(MongoStorage(email),blueprint)
-        if user is None:
-            raise ValueError("User not found")
-    except:
-        user=ct.insert(resp.json())
-        MongoStorage.set(MongoStorage(email),blueprint,token)
-        user=find_email(email)
-    if current_user.is_authenticated == False:
-        login_user(User(email))
+    google_logged_in(blueprint, google.token)
     form = MyPerson()
 
     r_results=[]
-    x = user
+    x = google.email
 
     if x is not None:
         page, per_page, offset = get_page_args(
