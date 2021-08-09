d79dd69da0aae32b1c4a8a99da85140cf29bb6e7
Reginald Campbell <reg.c.campbell@gmail.com>
2021-08-08 22:35:59 -0400

    decentralize v8

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, redirect, url_for, render_template, request, jsonify, f
 from flask_pymongo import PyMongo, ObjectId
 from flask_login import current_user, login_user, logout_user, login_required, LoginManager, UserMixin
 import os
+from flask_dance.contrib.google import make_google_blueprint, google
+from flask_dance.consumer.storage import BaseStorage
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'choosytable'
@@ -17,3 +19,14 @@ app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = os.environ.get(
 login_manager = LoginManager()
 login_manager.login_view = "google.login"
 
+blueprint = make_google_blueprint(
+    client_id=os.environ.get("GOOGLE_CLIENT_ID"),
+    client_secret=os.environ.get("GOOGLE_CLIENT_SECRET"),
+    scope=["profile", "email"],
+    offline=True,
+    reprompt_consent=True
+    )
+app.register_blueprint(blueprint, url_prefix="/login")
+
+from app.main import bp as main_blueprint
+app.register_blueprint(main_blueprint)
