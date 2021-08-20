c7de8df24bce011b7f2ab2f6510cc94416cc8a75
Reginald Campbell <reg.c.campbell@gmail.com>
2021-08-19 20:24:04 -0400

    decentralize v19

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from flask_dance.consumer.storage import BaseStorage
 from flask_login import UserMixin
-from app import ct, login_manager
+from app import ct, nav, login_manager, e, iel, igl, p, age, location
 from flask_pymongo import PyMongo, ObjectId
 from bson import json_util
 from datetime import datetime
@@ -8,24 +8,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, RadioField, SubmitField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
-
-from app import ct, nav, login_manager
-
-e = ['Black', 'Afro-Latino', 'Bahamian', 'Jamaican', 'African']
-iel = ['White','Asian','Latino','Black','Afro-Latino',
-'African','Indigenous People','Pacific Islander', 'Unspecified']
-igl = ['Female','Male','Transgender','Agender','Unspecified']
-p = [('software_engineer','Software Engineer'),('staff_engineer','Staff Engineer'),('lead_engineering','Lead Engineer'),
-('architect','Architect'),('software_engineer_mngr','Software Engineer Manager'),('technical_mngr','Technical Manager'),('technical_drtr','Technical Director'),
-('vp','VP'),('cto','CTO'),('network_engineer','Network Engineer'),('principal_architect','Principal Architect'),('qa_engineer','QA Engineer'),('sre','SRE'),('sdet','SDET'),
-('project_mngr','Project Manager'),('program_mngr','Program Manager'),('devops_engineer','DevOps Engineer'),('systems_admin','Systems Admin'),
-('dba','DBA'),('operations_engineer','Operations Engineer')]
-age = ['18-24','25-34','35-44','45-54','55-64','65-74','75+']
-location = ['AK','AL','AR','AS','AZ','CA''CO','CT','DC','DE',
-'FL','GA','GU','HI','IA','ID','IL','IN','KS','KY','LA','MA',
-'MD','ME','MI','MN','MO','MP','MS','MT','NC','ND','NE','NH','NJ',
-'NM','NV','NY','OH','OK','OR','PA','PR','RI','SC','SD','TN',
-'TX','UT','VA','VI','VT','WA','WI','WV','WY']
 
 class MongoStorage(BaseStorage):
     def __init__(self, email):
@@ -47,7 +29,6 @@ class MongoStorage(BaseStorage):
             {'email': self.email}, 
             {'$pull': {'email': self.email}}
         )
-
 
 
 class User(UserMixin):
